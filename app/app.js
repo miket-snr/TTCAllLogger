d3f913d116f578673769d27a2124dd414ff9043f
Mike Thomson <miket.snr@gmail.com>
2015-09-09 22:39:28 +0200

    ww

--- app/app.js
+++ app/app.js
@@ -4,9 +4,11 @@
     angular.module('templateApp', ['treeControl','ui.bootstrap',
         'ngStorage',
         'ngRoute',
-        'angular-loading-bar',
+        'angular-loading-bar'
        
-    ])
+    ]) .config(function ($compileProvider){
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+    })
         .constant('urls', {
             BASE: 'https://apps.bidvestfm.co.za/FM2GO/Telkom',
             BASE_API: 'https://apps.bidvestfm.co.za/FM2GO/Telkom'
